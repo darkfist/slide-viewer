d5fd69e047dbbea43cce8d7653b8e4d1b79eac1e
Nishant Ranjan <darkfist1998@gmail.com>
2019-03-11 00:43:43 +0530

    integrated google file viever for displaying slides on the webpage

--- src/slides/forms.py
+++ src/slides/forms.py
@@ -3,13 +3,33 @@ from django import forms
 class AddSlide(forms.Form):
 	title 		= forms.CharField()
 	description = forms.CharField(required=False)
-	slide 	= forms.FileField()
-	slide2 	= forms.FileField(required=False)
-	slide3 	= forms.FileField(required=False)
-	slide4 	= forms.FileField(required=False)
-	slide5 	= forms.FileField(required=False)
-	slide6	= forms.FileField(required=False)
-	slide7 	= forms.FileField(required=False)
-	slide8 	= forms.FileField(required=False)
-	slide9 	= forms.FileField(required=False)
-	slide10	= forms.FileField(required=False)
+	slide 	= forms.FileField(validators=[FileExtensionValidator(
+												allowed_extensions=['ppt', 'pptx', 'pps']
+												)])
+	slide2 	= forms.FileField(validators=[FileExtensionValidator(
+												allowed_extensions=['ppt', 'pptx', 'pps']
+												)], required=False)
+	slide3 	= forms.FileField(validators=[FileExtensionValidator(
+												allowed_extensions=['ppt', 'pptx', 'pps']
+												)], required=False)
+	slide4 	= forms.FileField(validators=[FileExtensionValidator(
+												allowed_extensions=['ppt', 'pptx', 'pps']
+												)], required=False)
+	slide5 	= forms.FileField(validators=[FileExtensionValidator(
+												allowed_extensions=['ppt', 'pptx', 'pps']
+												)], required=False)
+	slide6	= forms.FileField(validators=[FileExtensionValidator(
+												allowed_extensions=['ppt', 'pptx', 'pps']
+												)], required=False)
+	slide7 	= forms.FileField(validators=[FileExtensionValidator(
+												allowed_extensions=['ppt', 'pptx', 'pps']
+												)], required=False)
+	slide8 	= forms.FileField(validators=[FileExtensionValidator(
+												allowed_extensions=['ppt', 'pptx', 'pps']
+												)], required=False)
+	slide9 	= forms.FileField(validators=[FileExtensionValidator(
+												allowed_extensions=['ppt', 'pptx', 'pps']
+												)], required=False)
+	slide10	= forms.FileField(validators=[FileExtensionValidator(
+												allowed_extensions=['ppt', 'pptx', 'pps']
+												)], required=False)
--- src/slides/views.py
+++ src/slides/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from document_viewer.settings import CDN_DOMAIN
 
-# Create your views here.
 from .models import Slide, SlideUpload
 from .forms import AddSlide
 
@@ -13,11 +13,15 @@ def add_slides(request):
 	errors = None
 	if form.is_valid():
 		if request.user.is_authenticated():
+			
+			# looping through all file fields and saving it's name in a single string
 			all_sld_url = str(form.cleaned_data.get('slide'))
 			for i in range(2,11):
 				extra = 'slide' + str(i)
 				if (str(form.cleaned_data.get(extra)) != 'None'):
 					all_sld_url = all_sld_url + " " + str(form.cleaned_data.get(extra))
+			
+			# saving the details in the db
 			obj = Slide.objects.create(
 					title = form.cleaned_data.get('title'), 
 					description = form.cleaned_data.get('description'),
@@ -25,12 +29,15 @@ def add_slides(request):
 					uploaded_by = request.user
 				)
 
+			# uploading the first slide with same id
 			query = Slide.objects.latest('id')
 			obj = SlideUpload.objects.create(
 					slide_id = query.id,
 					name = form.cleaned_data.get('title'),
 					slide_url = form.cleaned_data.get('slide'),
 				)
+
+			# looping through all other slides and uploading with same id
 			for i in range(2,11):
 				extra = 'slide' + str(i)
 				if (str(form.cleaned_data.get(extra)) != 'None'):
@@ -51,8 +58,16 @@ def add_slides(request):
 
 def display_slides(request):
 	template_name = 'slides/display_slides.html'
+	
+	# querying Slide table from db
 	queryset1 = Slide.objects.all().order_by('-pk')
+	
+	# querying SlideUpload table from db
 	queryset2 = SlideUpload.objects.all()
+
+	# domain of CDN where the slides will be uploaded
+	cdn = CDN_DOMAIN
+
 	context = {"object_list": queryset1, "slide_list":queryset2}
 	return render(request, template_name, context)
 
@@ -60,15 +75,15 @@ def display_slides(request):
 @login_required(login_url='/login/')
 def user_slides(request):
 	template_name = 'slides/user_slides.html'
+	
+	# querying Slide table from db
 	queryset1 = Slide.objects.filter(uploaded_by=request.user).order_by('-pk')
+	
+	# querying SlideUpload table from db
 	queryset2 = SlideUpload.objects.all()
+
+	# domain of CDN where the slides will be uploaded
+	cdn = CDN_DOMAIN
+
 	context = {"object_list": queryset1, "slide_list":queryset2}
-	return render(request, template_name, context)
-
-
-def slide_details(request, slug):
-	template_name = 'slides/slide_details.html'
-	obj = get_object_or_404(Slide, slug=slug)
-	queryset = SlideUpload.objects.all()
-	context = {"object": obj, "slide_list":queryset}
 	return render(request, template_name, context)
